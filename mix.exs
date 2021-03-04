b0e78a15f0715a9c0ec41deacc7706136221c42c
George Lima <georgelima11@hotmail.com>
2021-03-03 23:14:28 -0300

    add poison

--- mix.exs
+++ mix.exs
@@ -42,7 +42,8 @@ defmodule CloudWatch.Mixfile do
       {:httpoison, "~> 0.11.1"},
       {:credo, "~> 0.4.13", only: :dev},
       {:mock, "~> 0.3.2", only: :test},
-      {:ex_doc, ">= 0.0.0", only: :dev}
+      {:ex_doc, ">= 0.0.0", only: :dev},
+      {:poison, "~> 3.1"}
     ]
   end
 
